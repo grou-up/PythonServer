c2387de9a084e434ce316b36036cd8b54ca0c6cf
boseok <fa7271@naver.com>
2024-12-15 23:41:29 +0900

    entity set

--- DjangoServer/lottoTest/models.py
+++ DjangoServer/lottoTest/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.db.models import Model
+
 
 class ExcelFile(models.Model):
     file = models.FileField(upload_to='excel_files/')
 
     class Meta:
         db_table = 'lotto'
-
+# 멤버 테이블
 class Member(models.Model):
     email = models.CharField(max_length=255, primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -18,55 +20,106 @@ class Member(models.Model):
     class Meta:
         managed = False  # Django가 이 테이블을 관리하지 않도록 설정
         db_table = 'member'
-
-class Record(models.Model):
-    date = models.CharField(max_length=255)
-    billing_method = models.CharField(max_length=255)
-    sales_method = models.CharField(max_length=255)
-    ad_type = models.CharField(max_length=255)
-    campaign_id = models.CharField(max_length=255)
-    campaign_name = models.CharField(max_length=255)
-    ad_group = models.CharField(max_length=255)
-    product_advertised = models.CharField(max_length=255)
-    option_id_advertised = models.CharField(max_length=255)
-    product_with_conversion_sales = models.CharField(max_length=255)
-    option_id_with_conversion_sales = models.CharField(max_length=255)
-    ad_placement = models.CharField(max_length=255)
-    keyword = models.CharField(max_length=255)
-    impressions = models.IntegerField()
-    clicks = models.IntegerField()
-    ad_cost = models.DecimalField(max_digits=15, decimal_places=2)
-    click_through_rate = models.DecimalField(max_digits=5, decimal_places=2)
-    total_orders_1_day = models.IntegerField()
-    direct_orders_1_day = models.IntegerField()
-    indirect_orders_1_day = models.IntegerField()
-    total_sales_quantity_1_day = models.IntegerField()
-    direct_sales_quantity_1_day = models.IntegerField()
-    indirect_sales_quantity_1_day = models.IntegerField()
-    total_conversion_revenue_1_day = models.DecimalField(max_digits=15, decimal_places=2)
-    direct_conversion_revenue_1_day = models.DecimalField(max_digits=15, decimal_places=2)
-    indirect_conversion_revenue_1_day = models.DecimalField(max_digits=15, decimal_places=2)
-    total_orders_14_days = models.IntegerField()
-    direct_orders_14_days = models.IntegerField()
-    indirect_orders_14_days = models.IntegerField()
-    total_sales_quantity_14_days = models.IntegerField()
-    direct_sales_quantity_14_days = models.IntegerField()
-    indirect_sales_quantity_14_days = models.IntegerField()
-    total_conversion_revenue_14_days = models.DecimalField(max_digits=15, decimal_places=2)
-    direct_conversion_revenue_14_days = models.DecimalField(max_digits=15, decimal_places=2)
-    indirect_conversion_revenue_14_days = models.DecimalField(max_digits=15, decimal_places=2)
-    total_ad_roi_1_day = models.DecimalField(max_digits=10, decimal_places=2)
-    direct_ad_roi_1_day = models.DecimalField(max_digits=10, decimal_places=2)
-    indirect_ad_roi_1_day = models.DecimalField(max_digits=10, decimal_places=2)
-    total_ad_roi_14_days = models.DecimalField(max_digits=10, decimal_places=2)
-    direct_ad_roi_14_days = models.DecimalField(max_digits=10, decimal_places=2)
-    indirect_ad_roi_14_days = models.DecimalField(max_digits=10, decimal_places=2)
-    campaign_start_date = models.CharField(max_length=255)
-    member = models.ForeignKey(Member, on_delete=models.CASCADE)  # Member와의 1:N 단방향 관계 설정
+# 캠페인 테이블
+class Campaign(models.Model):
+    campaign_id = models.CharField(max_length=255,primary_key=True)
+    cam_ad_type = models.CharField(max_length=255)
+    cam_campaign_name = models.CharField(max_length=255)
+    email = models.ForeignKey(Member, on_delete=models.CASCADE, db_column='email')
 
     def __str__(self):
-        return f"Campaign ID: {self.campaign_id}, Date: {self.date}"
+        return f"Campaign ID: {self.campaign_id}, Name: {self.cam_campaign_name}"
 
     class Meta:
-        db_table = 'record'
+        db_table = 'campaign'
+# 광고 실행 ㅌ[ㅇ;ㅂ,ㄹ
+class Execution(models.Model):
+    execution_id = models.CharField(max_length=255, primary_key=True)
+    exe_product_name = models.CharField(max_length=255)
+    exe_detail_category = models.CharField(max_length=255)
+    campaign_id = models.ForeignKey(Campaign, on_delete=models.CASCADE, db_column='campaign_id')
+
+    def __str__(self):
+        return f"Execution ID: {self.execution_id}, Product: {self.exe_product_name}"
+
+    class Meta:
+        db_table = 'execution'
+# 키워드 테이블
+class Keyword(models.Model):
+    keyword_id = models.AutoField(primary_key=True)  # 기본 키로 설정
+    key_keyword = models.CharField(max_length=255)  # 키워드
+    key_impressions = models.BigIntegerField()  # 노출수
+    key_clicks = models.BigIntegerField()  # 클릭수
+    key_click_rate = models.FloatField()  # 클릭률
+    key_total_sales = models.BigIntegerField()  # 총 주문수
+    key_cvr = models.FloatField()  # 전환율
+    key_cpc = models.FloatField()  # CPC
+    key_adcost = models.FloatField()  # 광고비
+    key_adsales = models.FloatField()  # 광고매출
+    key_roas = models.FloatField()  # ROAS
+    key_date = models.DateField()  # 날짜
+    key_exclude_flag = models.BooleanField(default=False)  # 제외여부
+
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE, db_column='campaignId')
+
+    def __str__(self):
+        return f"Keyword ID: {self.keyword_id}, Keyword: {self.key_keyword}"
+
+    class Meta:
+        db_table = 'keyword'
+
+# 캠페인 옵션 디테일 테이블
+class CampaignOptionDetails(models.Model):
+    id = models.AutoField(primary_key=True)  # 기본 키로 설정
+    cop_date = models.DateField()  # 날짜
+    cop_impressions = models.BigIntegerField()  # 노출수
+    cop_sales = models.BigIntegerField()  # 주문수
+    cop_adcost = models.FloatField()  # 광고비
+    cop_adsales = models.FloatField()  # 광고매출
+    cop_roas = models.FloatField()  # ROAS
+    cop_clicks = models.BigIntegerField()  # 클릭수
+    cop_click_rate = models.FloatField()  # 클릭률
+    cop_cvr = models.FloatField()  # 전환율
+    cop_search_type = models.CharField(max_length=255)  # 검색 비검색
+
+    execution = models.ForeignKey(Execution, on_delete=models.CASCADE, db_column='executionId')
+
+    def __str__(self):
+        return f"Campaign Option Detail ID: {self.id}"
+    class Meta:
+        db_table = 'campaign_option_details'
+
+# 메모 테이블
+class Memo(models.Model):
+    id = models.AutoField(primary_key=True)  # 기본 키로 설정
+    mem_date = models.DateField()  # 날짜 (시간은 필요 없음)
+    mem_content = models.TextField()  # 메모 내용
+
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE, db_column='campaignId')
+
+    def __str__(self):
+        return f"Memo ID: {self.id}"
+    class Meta:
+        db_table = 'memo'
+
+class Margin(models.Model):
+    id = models.AutoField(primary_key=True)  # 기본 키
+    mar_campaign_id = models.CharField(max_length=255)  # 캠페인 ID
+    mar_date = models.DateField()  # 날짜
+    mar_target_efficiency = models.FloatField()  # 목표 효율성
+    mar_ad_revenue = models.FloatField()  # 광고 수익
+    mar_ad_budget = models.FloatField()  # 광고 예산
+    mar_ad_cost_ratio = models.FloatField()  # 광고비 비율
+    mar_cpc_unit_cost = models.FloatField()  # CPC 단가
+    mar_impressions = models.BigIntegerField()  # 노출 수
+    mar_clicks = models.BigIntegerField()  # 클릭 수
+    mar_ad_conversion_sales = models.BigIntegerField()  # 광고 전환 판매 수
+    mar_actual_sales = models.BigIntegerField()  # 실제 판매 수
+    mar_ad_merge_count = models.BigIntegerField()  # 광고 머지 수
+    mar_net_profit = models.FloatField()  # 순이익
+
+    def __str__(self):
+        return f"Margin ID: {self.id}"
+    class Meta:
+        db_table = 'margin'
 
