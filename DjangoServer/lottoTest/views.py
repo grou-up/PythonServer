abe861549abb97f9b50ce3ecb404120d495cd6a9
boseok <fa7271@naver.com>
2024-12-18 13:36:07 +0900

    [feat] views.py add

--- DjangoServer/lottoTest/views.py
+++ DjangoServer/lottoTest/views.py
@@ -1,16 +1,17 @@
-import pandas as pd
+from datetime import datetime
 import time
-from decimal import Decimal
-from django.shortcuts import render
+
+import pandas as pd
+from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
-
-from .models import Record, Member
+from django.db import transaction
+from .models import Member, Campaign, Execution, CampaignOptionDetails, Keyword
 from .forms import ExcelFileForm
-
-# 토큰 파싱
 from django.http import JsonResponse
 import jwt
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
@@ -18,107 +19,196 @@ def upload_excel(request):
     if request.method == 'POST':
         form = ExcelFileForm(request.POST, request.FILES)
         if form.is_valid():
-
-            email = some_protected_view(request)
-            member = get_object_or_404(Member, email=email)
-            print(
-                f"Member Info: Email={member.email}, Name={member.name}, Role={member.role}, CreatedAt={member.created_at}, UpdatedAt={member.updated_at}")
-
-            excel = pd.read_excel(request.FILES['file'])
-
-            # 각 행을 모델 인스턴스로 변환
-            start_time = time.time() * 1000  # 밀리세컨드 단위
-            records = []
-
-            for index, row in excel.iterrows():
-                # %가 포함된 문자열을 Decimal로 변환
-                def convert_percentage(value):
-                    if isinstance(value, str) and '%' in value:
-                        return Decimal(value.replace('%', '').strip()) / 100
-                    return Decimal(value)
-
-                record = Record(
-                    date=row['날짜'],
-                    billing_method=row['과금방식'],
-                    sales_method=row['판매방식'],
-                    ad_type=row['광고유형'],
-                    campaign_id=row['캠페인 ID'],
-                    campaign_name=row['캠페인명'],
-                    ad_group=row['광고그룹'],
-                    product_advertised=row['광고집행 상품명'],
-                    option_id_advertised=row['광고집행 옵션ID'],
-                    product_with_conversion_sales=row['광고전환매출발생 상품명'],
-                    option_id_with_conversion_sales=row['광고전환매출발생 옵션ID'],
-                    ad_placement=row['광고 노출 지면'],
-                    keyword=row['키워드'],
-                    impressions=row['노출수'],
-                    clicks=row['클릭수'],
-                    ad_cost=Decimal(row['광고비']),
-                    click_through_rate=convert_percentage(row['클릭률']),
-                    total_orders_1_day=row['총 주문수(1일)'],
-                    direct_orders_1_day=row['직접 주문수(1일)'],
-                    indirect_orders_1_day=row['간접 주문수(1일)'],
-                    total_sales_quantity_1_day=row['총 판매수량(1일)'],
-                    direct_sales_quantity_1_day=row['직접 판매수량(1일)'],
-                    indirect_sales_quantity_1_day=row['간접 판매수량(1일)'],
-                    total_conversion_revenue_1_day=Decimal(row['총 전환매출액(1일)']),
-                    direct_conversion_revenue_1_day=Decimal(row['직접 전환매출액(1일)']),
-                    indirect_conversion_revenue_1_day=Decimal(row['간접 전환매출액(1일)']),
-                    total_orders_14_days=row['총 주문수(14일)'],
-                    direct_orders_14_days=row['직접주문수(14일)'],
-                    indirect_orders_14_days=row['간접 주문수(14일)'],
-                    total_sales_quantity_14_days=row['총 판매수량(14일)'],
-                    direct_sales_quantity_14_days=row['직접 판매수량(14일)'],
-                    indirect_sales_quantity_14_days=row['간접 판매수량(14일)'],
-                    total_conversion_revenue_14_days=Decimal(row['총 전환매출액(14일)']),
-                    direct_conversion_revenue_14_days=Decimal(row['직접 전환매출액(14일)']),
-                    indirect_conversion_revenue_14_days=Decimal(row['간접 전환매출액(14일)']),
-                    total_ad_roi_1_day=convert_percentage(row['총광고수익률(1일)']),
-                    direct_ad_roi_1_day=convert_percentage(row['직접광고수익률(1일)']),
-                    indirect_ad_roi_1_day=convert_percentage(row['간접광고수익률(1일)']),
-                    total_ad_roi_14_days=convert_percentage(row['총광고수익률(14일)']),
-                    direct_ad_roi_14_days=convert_percentage(row['직접광고수익률(14일)']),
-                    indirect_ad_roi_14_days=convert_percentage(row['간접광고수익률(14일)']),
-                    campaign_start_date=row['캠페인 시작일'],
-                    member = member
-
-                )
-
-                records.append(record)
-
-            Record.objects.bulk_create(records)  # 데이터를 한 번에 삽입
-            total_records = Record.objects.count()
-            print(f"total count: {total_records}")
-
-            end_time = time.time() * 1000
-            time_taken = end_time - start_time
-            print(f"Time taken: {time_taken} milliseconds")
-            return render(request, 'upload_excel.html', {'form': form, 'data': excel.to_html()})
+            return handle_excel_upload(request, form)
     else:
         form = ExcelFileForm()
     return render(request, 'upload_excel.html', {'form': form})
 
 
+def handle_excel_upload(request, form):
+    start_time = time.time() * 1000  # 밀리세컨드 단위
+
+    email = some_protected_view(request)
+    member = get_object_or_404(Member, email=email)
+    logger.info(f"Member Info: Email={member.email}")
+
+    excel = pd.read_excel(request.FILES['file'], dtype={'날짜': str})
+    aggregated_data, keyword_data = aggregate_data(excel, member)
+
+    with transaction.atomic():  # 트랜잭션 시작
+        save_campaign_option_details(aggregated_data)
+        save_keywords(keyword_data)
+
+    end_time = time.time() * 1000
+    time_taken = end_time - start_time
+    print(f"Time taken: {time_taken} milliseconds")
+
+    return render(request, 'upload_excel.html', {'form': form, 'data': excel.to_html()})
+
+
+def aggregate_data(excel, member):
+    aggregated_data = {}
+    keyword_data = {}
+
+    for index, row in excel.iterrows():
+        campaign = get_or_create_campaign(row, member)
+        execution = get_or_create_execution(row, campaign)
+
+        cop_date = datetime.strptime(str(row['날짜']), '%Y%m%d').date()
+        cop_search_type = row['광고 노출 지면']
+
+        detail_key = (cop_date, execution.execution_id, cop_search_type)
+        update_aggregated_data(aggregated_data, detail_key, row, execution, cop_date, cop_search_type)
+
+        key_keyword = row['키워드']
+        keyword_key = (cop_date, campaign.campaign_id, key_keyword)
+        update_keyword_data(keyword_data, keyword_key, row, campaign, cop_date,key_keyword, cop_search_type)
+
+    return aggregated_data, keyword_data
+
+
+def update_aggregated_data(aggregated_data, detail_key, row, execution, cop_date, cop_search_type):
+    if detail_key not in aggregated_data:
+        aggregated_data[detail_key] = {
+            'cop_date': cop_date,
+            'execution': execution,
+            'cop_impressions': 0,
+            'cop_clicks': 0,
+            'cop_adcost': 0,
+            'cop_sales': 0,
+            'cop_adsales': 0,
+            'cop_roas': 0,
+            'cop_cvr': 0,
+            'cop_click_rate': 0,
+            'cop_search_type': cop_search_type
+        }
+        # 집계
+    aggregated_data[detail_key]['cop_impressions'] += row['노출수']
+    aggregated_data[detail_key]['cop_clicks'] += row['클릭수']
+    aggregated_data[detail_key]['cop_adcost'] += row['광고비']
+    aggregated_data[detail_key]['cop_sales'] += row['총 판매수량(1일)']
+    aggregated_data[detail_key]['cop_adsales'] += row['총 전환매출액(1일)']
+
+
+def update_keyword_data(keyword_data, keyword_key, row, campaign, cop_date,key_keyword,cop_search_type):
+    key_exclude_flag = row.get('제외여부', False)
+    if keyword_key not in keyword_data:
+        keyword_data[keyword_key] = {
+            'campaign': campaign,
+            'key_keyword': key_keyword,
+            'key_impressions': 0,
+            'key_clicks': 0,
+            'key_click_rate': 0,
+            'key_total_sales': 0,
+            'key_cvr': 0,
+            'key_cpc': 0,
+            'key_adcost': 0,
+            'key_adsales': 0,
+            'key_roas': 0,
+            'key_date': cop_date,
+            'key_exclude_flag': key_exclude_flag,
+            'key_search_type': cop_search_type,
+        }
+
+    keyword_data[keyword_key]['key_impressions'] += row['노출수']
+    keyword_data[keyword_key]['key_clicks'] += row['클릭수']
+    keyword_data[keyword_key]['key_adcost'] += row['광고비']
+    keyword_data[keyword_key]['key_total_sales'] += row['총 판매수량(1일)']
+    keyword_data[keyword_key]['key_adsales'] += row['총 전환매출액(1일)']
+
+
+def save_campaign_option_details(aggregated_data):
+    buffer = []
+    for key, data in aggregated_data.items():
+        if not CampaignOptionDetails.objects.filter(
+                execution=data['execution'],
+                cop_date=data['cop_date'],
+                cop_search_type=data['cop_search_type']
+        ).exists():
+            buffer.append(CampaignOptionDetails(
+                cop_date=data['cop_date'],
+                cop_impressions=data['cop_impressions'],
+                cop_sales=data['cop_sales'],
+                cop_adcost=data['cop_adcost'],
+                cop_adsales=data['cop_adsales'],
+                cop_roas=(data['cop_adsales'] / data['cop_adcost']) * 100 if data['cop_adcost'] > 0 else 0,
+                cop_clicks=data['cop_clicks'],
+                cop_cvr=round((data['cop_sales'] / data['cop_clicks']) * 100, 2) if data['cop_clicks'] > 0 else 0,
+                cop_click_rate=(data['cop_clicks'] / data['cop_impressions']) * 100 if data['cop_impressions'] > 0 else 0,
+                cop_search_type=data['cop_search_type'],
+                execution=data['execution']
+            ))
+    CampaignOptionDetails.objects.bulk_create(buffer)
+
+
+def save_keywords(keyword_data):
+    buffer = []
+    for key, data in keyword_data.items():
+        if not Keyword.objects.filter(
+                key_keyword=data['key_keyword'],
+                key_date=data['key_date'],
+                campaign=data['campaign']
+        ).exists():
+            buffer.append(Keyword(
+                key_keyword=data['key_keyword'],
+                key_impressions=data['key_impressions'],
+                key_clicks=data['key_clicks'],
+                key_click_rate=round((data['key_clicks'] / data['key_impressions']) * 100, 2) if data[
+                                                                                                     'key_impressions'] > 0 else 0,
+                key_total_sales=data['key_total_sales'],
+                key_cvr=round((data['key_total_sales'] / data['key_clicks']) * 100, 2) if data[
+                                                                                              'key_clicks'] > 0 else 0,
+                key_cpc=round((data['key_adcost'] * 1.1) / data['key_clicks']) if data[
+                                                                                      'key_clicks'] > 0 else 0,
+                key_adcost=round(data['key_adcost'] * 1.1),
+                key_adsales=data['key_adsales'],
+                key_roas=round(data['key_adsales'] / (data['key_adcost'] * 1.1), 2) * 100 if data[
+                                                                                                 'key_adcost'] > 0 else 0,
+                key_date=data['key_date'],
+                key_exclude_flag=data['key_exclude_flag'],
+                key_search_type=data['key_search_type'],
+                campaign=data['campaign']
+
+            ))
+    Keyword.objects.bulk_create(buffer)
+
+
+def get_or_create_campaign(row, member):
+    campaign_id = row['캠페인 ID']
+    campaign, created = Campaign.objects.get_or_create(
+        campaign_id=campaign_id,
+        defaults={
+            'cam_campaign_name': row['캠페인명'],
+            'cam_ad_type': row['광고유형'],
+            'email': member
+        }
+    )
+    return campaign
+
+
+def get_or_create_execution(row, campaign):
+    execution_id = row['광고집행 옵션ID']
+    execution, created = Execution.objects.get_or_create(
+        execution_id=execution_id,
+        defaults={
+            'exe_product_name': row['광고집행 상품명'],
+            'exe_detail_category': "",
+            'campaign_id': campaign,
+        }
+    )
+    return execution
+
+
 def some_protected_view(request):
     auth_header = request.headers.get('Authorization', None)
-
     if auth_header is None:
         return JsonResponse({'error': 'Authorization header missing'}, status=401)
 
     try:
-        # Bearer <token> 형태이므로 "Bearer " 부분을 제거
         token = auth_header.split(' ')[1]
-
-        # 토큰 해석 (서명 검증 없이)
         decoded_payload = jwt.decode(token, options={"verify_signature": False})
-
-        # 디코딩된 토큰 내용 출력
-        print(f"Token decoded: {decoded_payload}")
-
-        # 'sub'에서 이메일만 추출
         sub_value = decoded_payload['sub']
         email = sub_value.split(':')[0]
-
         return email
     except jwt.InvalidTokenError:
         return JsonResponse({'error': 'Invalid token'}, status=401)
